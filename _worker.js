1ee09d2b62bccd0af442d099031ab04946f4f4eb
bgenting <82087159+bgenting@users.noreply.github.com>
2023-08-30 10:36:51 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,9 +5,9 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'ccdecf77-1e79-433e-a1f8-20db51eb868d';
 
-const proxyIPs = ['cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org'];
+const proxyIPs = ['www.visa.de'];
 
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
